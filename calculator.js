406b9afb0cc1142d00b7be9941225f7136035e17
peuchen <npeuchen@gmail.com>
2020-07-13 18:35:19 +0200

    Expand operate function

--- calculator.js
+++ calculator.js
@@ -14,26 +14,49 @@ function changeDisplay() {
 };
 
 function operate(operator, currentValue, newValue) {
-    return operator(currentValue, newValue);
+    operators.forEach((operator) => {
+        operator.addEventListener('click', (e) => {
+            switch(operator.id) {
+                case "add":
+                    add();
+                    break;
+                case "subtract":
+                    subtract();
+                    break;
+                case "multiply":
+                    multiply();
+                    break;
+                case "divide":
+                    divide();
+                    break;
+            };
+        });
+    });
+    
 }
 
-function add (currentValue, newValue) {
-	return currentValue + newValue;
+const add = function(currentValue, newValue) {
+    console.log("test1");
+    return currentValue + newValue;
 }
 
-function subtract (currentValue, newValue) {
-	return currentValue - newValue;
+const subtract = function(currentValue, newValue) {
+    console.log("test2");
+    return currentValue - newValue;
 }
 
-function multiply (currentValue, newValue) {
-	return currentValue * newValue;
+const multiply = function(currentValue, newValue) {
+    console.log("test3");
+    return currentValue * newValue;
 }
 
-function divide (currentValue, newValue) {
-	return currentValue / newValue;
+const divide = function(currentValue, newValue) {
+    console.log("test4");
+    return currentValue / newValue;
 }
 
 changeDisplay();
+operate();
 
 /* function multiply (array) {
 	return array.reduce(((total, newValue) => total * newValue), 1);
