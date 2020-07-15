da8ed48b3fc9ba30df1a3a94370002cbc9aafb12
peuchen <npeuchen@gmail.com>
2020-07-15 11:52:51 +0200

    Going to revamp all code

--- calculator.js
+++ calculator.js
@@ -1,63 +1,118 @@
-const numbers = document.querySelectorAll('.number');
+const numbers = document.querySelectorAll('.operand');
 const operators = document.querySelectorAll('.operator');
-let currentValue = 0;
-let newValue = 0;
+const display = document.getElementById("display");
+let displayValue = '0'
+let firstOperand = null;
+let secondOperand = null;
+let firstOperator = null;
+let secondOperator = null;
+let result = 0;
+
+
 
 function changeDisplay() {
-    numbers.forEach((number) => {
-        number.addEventListener('click', (e) => {
-            let currentNumber = number.textContent;
-            document.getElementById("display").textContent += currentNumber;
-            newValue = Number(document.getElementById("display").textContent);
-        });
-    });
+    display.textContent = displayValue;
 };
 
-function operate(operator, currentValue, newValue) {
+changeDisplay();
+
+function operate() {
     operators.forEach((operator) => {
         operator.addEventListener('click', (e) => {
-            currentValue = Number(document.getElementById("display").textContent);
-            switch(operator.id) {
-                case "add":
-                    add();
-                    break;
-                case "subtract":
-                    subtract();
-                    break;
-                case "multiply":
-                    multiply();
-                    break;
-                case "divide":
-                    divide();
-                    break;
-            };
+            currentValues.push(Number(display.textContent));
+            console.log(currentValues);
+            if(!action) {
+                action = operator.id;
+            }
+            else if (action) {
+                calculate();
+                action = operator.id;
+                display.textContent = result;
+            }
         });
     });
-    
+
+};
+
+function inputOperand() {
+    numbers.forEach((operand) => {
+        operand.addEventListener('click', (e) => {
+            if(firstOperator === null) {
+                if(displayValue === '0' || displayValue === 0) {
+                    //1st click - handles first operand input
+                    displayValue = operand;
+                } else if(displayValue === firstOperand) {
+                    //starts new operation after inputEquals()
+                    displayValue = operand;
+                } else {
+                    displayValue += operand;
+                }
+            } 
+            else {
+                //3rd/5th click - inputs to secondOperand
+                if(displayValue === firstOperand) {
+                    displayValue = operand;
+                } else {
+                    displayValue += operand;
+                }
+            }
+        });
+        changeDisplay();
+    });
 }
 
-const add = function(currentValue, newValue) {
+const calculate = function() {
+    switch(action) {
+        case "add":
+            add();
+            break;
+        case "subtract":
+            subtract();
+            break;
+        case "multiply":
+            multiply();
+            break;
+        case "divide":
+            divide();
+            break;
+    };
+};
+
+const add = function() {
     console.log("test1");
-    return currentValue + newValue;
+    reducer = (accumulator, currentValue) => accumulator + currentValue;
+    result = currentValues.reduce(reducer);
+    currentValues = [];
+    currentValues.push(Number(display.textContent));
 }
 
-const subtract = function(currentValue, newValue) {
+const subtract = function() {
     console.log("test2");
-    return currentValue - newValue;
+    reducer = (accumulator, currentValue) => accumulator - currentValue;
+    result = currentValues.reduce(reducer);
+    currentValues = [];
+    currentValues.push(Number(display.textContent));
 }
 
-const multiply = function(currentValue, newValue) {
+const multiply = function() {
     console.log("test3");
-    return currentValue * newValue;
+    reducer = (accumulator, currentValue) => accumulator * currentValue;
+    result = currentValues.reduce(reducer);
+    currentValues = [];
+    currentValues.push(Number(display.textContent));
 }
 
-const divide = function(currentValue, newValue) {
+const divide = function() {
     console.log("test4");
-    return currentValue / newValue;
+    reducer = (accumulator, currentValue) => accumulator / currentValue;
+    result = currentValues.reduce(reducer);
+    currentValues = [];
+    currentValues.push(Number(display.textContent));
 }
 
-changeDisplay();
+
 operate();
+
 
 /* function multiply (array) {
 	return array.reduce(((total, newValue) => total * newValue), 1);
